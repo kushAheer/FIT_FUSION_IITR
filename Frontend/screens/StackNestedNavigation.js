d914bdfcc5c7c1648e6fc64e2f0f6a23656f7fd7
Kush Aheer <kushaheer709@gmail.com>
2025-03-21 02:02:47 +0530

    Fixed Error

--- Frontend/screens/StackNestedNavigation.js
+++ Frontend/screens/StackNestedNavigation.js
@@ -7,39 +7,32 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 const Stack = createStackNavigator();
 
 export function ScheduleNestedNavigation({ navigation }) {
-    
-    
-
-
-  return (
-    <Stack.Navigator
-      initialRouteName="ScheduleScreen"
-    >
-		
-      <Stack.Screen
-        name="ScheduleScreen"
-        component={ScheduleScreen}
-        options={{
-          headerShown: false,
-        }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="ScheduleDetailScreen"
-        component={ScheduleDetailScreen}
-        options={{
-          headerShown: true,
-		  headerTitle: "",
-          headerRight: ({color}) => (
-            <Ionicons
-			  name="pencil-outline"
-			  size={30}
-			  color={color}
-			  style={{ marginRight: 20 }}
-			//   onPress={() => navigation.goBack()}
+	return (
+		<Stack.Navigator initialRouteName="ScheduleScreen">
+			<Stack.Screen
+				name="ScheduleScreen"
+				component={ScheduleScreen}
+				options={{
+					headerShown: false,
+				}}
+			></Stack.Screen>
+			<Stack.Screen
+				name="ScheduleDetailScreen"
+				component={ScheduleDetailScreen}
+				options={{
+					headerShown: true,
+					headerTitle: "",
+					headerRight: ({ color }) => (
+						<Ionicons
+							name="pencil-outline"
+							size={30}
+							color={color}
+							style={{ marginRight: 20 }}
+							//   onPress={() => navigation.goBack()}
+						/>
+					),
+				}}
 			/>
-          ),
-        }}
-      />
-    </Stack.Navigator>
-  );
+		</Stack.Navigator>
+	);
 }
