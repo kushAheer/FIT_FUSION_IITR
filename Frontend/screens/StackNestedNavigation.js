4d3d69d8adeed49c7228cdaffaee6924b42af94d
Kush Aheer <kushaheer709@gmail.com>
2025-03-22 12:27:43 +0530

    Nutrition Navigation Done

--- Frontend/screens/StackNestedNavigation.js
+++ Frontend/screens/StackNestedNavigation.js
@@ -3,6 +3,7 @@ import ScheduleScreen from "./ScheduleScreen";
 import ScheduleDetailScreen from "./ScheduleDetailScreen";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
+import NutritionScreen from "./NutritionScreen";
 
 const Stack = createStackNavigator();
 
@@ -18,6 +19,37 @@ export function ScheduleNestedNavigation({ navigation }) {
 			></Stack.Screen>
 			<Stack.Screen
 				name="ScheduleDetailScreen"
+				component={ScheduleDetailScreen}
+				options={{
+					headerShown: true,
+					headerTitle: "",
+					headerRight: ({ color }) => (
+						<Ionicons
+							name="pencil-outline"
+							size={30}
+							color={color}
+							style={{ marginRight: 20 }}
+							//   onPress={() => navigation.goBack()}
+						/>
+					),
+				}}
+			/>
+		</Stack.Navigator>
+	);
+}
+
+export function NutritionNestedNavigation({ navigation }) {
+	return (
+		<Stack.Navigator initialRouteName="NutritionScreen">
+			<Stack.Screen
+				name="NutritionScreen"
+				component={NutritionScreen}
+				options={{
+					headerShown: false,
+				}}
+			></Stack.Screen>
+			<Stack.Screen
+				name="NutritionDetailScreen"
 				component={ScheduleDetailScreen}
 				options={{
 					headerShown: true,
